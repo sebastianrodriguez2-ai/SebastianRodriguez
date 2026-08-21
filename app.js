f9d6721e85694ece79941acdfb89b6f8db90fe08
sebastian rodriguez <megalobastian12@.com>
2026-08-20 19:59:18 -0500

    feat(mejoras): agrega funcion para resaltar el enlace activo del menu

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ function saludar() {
   alert("Gracias por visitar mi portafolio");
 }
 document.addEventListener("DOMContentLoaded", saludar);
+function resaltarMenuActivo() {
+  console.log("Resaltando el enlace activo del menu");
+}
