5bfa827dca886a60554c771616489fdf136c35cd
sebastian rodriguez <megalobastian12@.com>
2026-08-20 20:02:18 -0500

    fix(optimizacion): corrige la optimizacion conectando la funcion correcta al evento

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("Portafolio cargado correctamente");
 function saludar() {
   alert("Gracias por visitar mi portafolio");
 }
-document.addEventListener("DOMContentLoaded", saludar);
+document.addEventListener("DOMContentLoaded", saludarUnaVez);
 function resaltarMenuActivo() {
   console.log("Resaltando el enlace activo del menu");
 }
